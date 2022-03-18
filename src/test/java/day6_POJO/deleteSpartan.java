8877693d259dbb08b7bce5ed140b01476c310235
ihsankykc <ihsankykc@gmail.com>
2022-03-18 15:44:18 +0100

    day8

--- src/test/java/day6_POJO/deleteSpartan.java
+++ src/test/java/day6_POJO/deleteSpartan.java
@@ -18,11 +18,32 @@ public class deleteSpartan {
         baseURI= ConfigurationReader.get("spartan_api_url");
     }
 
+
     @Test
-    public void deleteSpartan(){
+    public void deleteOneSpartan(){
+        int id=302;
+
+        Response response=given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().pathParam("id",id)
+                .when().delete("api/spartans/{id}");
+
+        assertEquals(response.statusCode(),204);
+
+        Response response1=given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().pathParam("id",id)
+                .when().get("api/spartans/{id}");
+
+        assertEquals(response1.statusCode(),404);
+
+    }
+
+    @Test
+    public void deleteSomeSpartan(){
 
         List<Integer>ids=new ArrayList<>();
-        for (int i = 170; i < 280; i++) {
+        for (int i = 140; i < 141; i++) {
              ids.add(i);
         }
 
